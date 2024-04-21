cdbe0695c5a04b53018e75110f265dae250bd31c
ArmanKhalili82 <hykhalili@yahoo.com>
2024-04-21 14:43:53 +0200

    Ajax

--- CountryJQueryAjax/Controllers/CountryController.cs
+++ CountryJQueryAjax/Controllers/CountryController.cs
@@ -25,46 +25,92 @@ namespace CountryJQueryAjax.Controllers
             return Ok(ItemCountryList);
         }
 
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
+        [HttpPost]
+        public async Task<ActionResult> Create(CountryInfo model)
         {
-            CountryInfo item = await _context.Countries.FindAsync(id);
-            if (item == null)
+            if (ModelState.IsValid)
             {
-                return NotFound();
+                await _context.Countries.AddAsync(model);
+                await _context.SaveChangesAsync();
+                return Json("Country infos saved");
             }
-            _context.Countries.Remove(item);
-
-            return RedirectToAction("Index");
+            return Json("Validation failed");
         }
 
         [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost, ActionName("Create")]
-        public async Task<IActionResult> CreatePost(CountryInfo obj)
-        {
-            _context.Countries.Add(obj);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
             var item = await _context.Countries.FindAsync(id);
-            return View(item);
+            return Json(item);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(CountryInfo obj)
+        public async Task<ActionResult> Update(CountryInfo model)
         {
-            _context.Countries.Update(obj);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            if(ModelState.IsValid)
+            {
+                _context.Countries.Update(model);
+                await _context.SaveChangesAsync();
+                return Json("Country infos updated");
+            }
+            return Json("Validation failed");
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var item = await _context.Countries.FindAsync(id);
+
+            if (item != null)
+            {
+                _context.Countries.Remove(item);
+                await _context.SaveChangesAsync();
+                return Json("Country infos deleted");
+            }
+            return Json($"Country infos not found with id {id}");
+        }
+
+
+        //[HttpDelete]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    CountryInfo item = await _context.Countries.FindAsync(id);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    _context.Countries.Remove(item);
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost, ActionName("Create")]
+        //public async Task<IActionResult> CreatePost(CountryInfo obj)
+        //{
+        //    _context.Countries.Add(obj);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpGet]
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var item = await _context.Countries.FindAsync(id);
+        //    return View(item);
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(CountryInfo obj)
+        //{
+        //    _context.Countries.Update(obj);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
--- CountryJQueryAjax/wwwroot/js/Custom.js
+++ CountryJQueryAjax/wwwroot/js/Custom.js
@@ -2,6 +2,7 @@
     ShowAllData();
 })
 
+//Get Data
 function ShowAllData() {
     $.ajax({
         url: 'Country/CountryList',
@@ -16,8 +17,8 @@ function ShowAllData() {
                 object += '<td>' + item.city + '</td>';
                 object += '<td>' + item.capitalCity + '</td>';
                 object += '<td>' + item.population + '</td>';
-                object += '<td><a onclick="DelteId()" class="btn btn-danger">Delete</a></td>';
-                object += '<td><a onclick="EditId()" class="btn btn-primary">Edit</a></td>';
+                object += '<td><a onclick="DelteId(' + item.id + ')" class="btn btn-danger">Delete</a></td>';
+                object += '<td><a onclick="EditId(' + item.id + ')" class="btn btn-primary">Edit</a></td>';
                 object += '</tr>';
             });
             $('#table_data').html(object);
@@ -28,18 +29,213 @@ function ShowAllData() {
     });
 };
 
-function DelteId(id) {
+
+$(document).ready(function () {
+    $('#btnAdd').Click(function () {
+        alert("clicked");
+        $('#CountryModal').modal('show');
+        $('#modalTitle').text('Add Country');
+    });
+});
+
+
+//Add Data
+function Create() {
+    var result = Validate();
+    if (result == false) {
+        return false;
+    }
+
+
+    var formData = new Object();
+    formData.id = $('#Id').val();
+    formData.Country = $('#Country').val();
+    formData.City = $('#City').val();
+    formData.Population = $('#Population').val();
+    formData.CapitalCity = $('#CapitalCity').val();
+
     $.ajax({
-        url: `Country/Delete/${id}`,
-        type: "Delete"
-    })
+        url: '/Country/Create',
+        data: formData,
+        type: 'Post',
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                alert('Unable to save the data');
+            }
+            else {
+                HideModal();
+                ShowAllData();
+                alert(response);
+            }
+        },
+        error: function () {
+            alert('Unable to save the data');
+        }
+
+    });
 }
 
-function EditId() {
-    window.location.href = "/country/create";
-    $.ajax({
-        url: 'Country/Edit',
-        type: "Get"
-    })
-
+function HideModal() {
+    $(document).ready(function () {
+        ClearData();
+        $('#btnAdd').Click(function () {
+            $('#CountryModal').modal('hide');
+        });
+    });
 }
+
+function ClearData() {
+    $('#Country').val('');
+    $('#City').val('');
+    $('#Population').val('');
+
+    $('#Country').css('border-color', 'lightgrey');
+    $('#City').css('border-color', 'lightgrey');
+    $('#Population').css('border-color', 'lightgrey');
+}
+
+function Validate() {
+    var isValid = true;
+
+    if ($('#Country').val().trim() == "") {
+        $('#Country').css('border-color', 'red');
+        isValid = false;
+    }
+    else {
+        $('#Country').css('border-color', 'lightgrey');
+    }
+
+    if ($('#City').val().trim() == "") {
+        $('#City').css('border-color', 'red');
+        isValid = false;
+    }
+    else {
+        $('#City').css('border-color', 'lightgrey');
+    }
+
+    if ($('#Population').val().trim() == "") {
+        $('#Population').css('border-color', 'red');
+        isValid = false;
+    }
+    else {
+        $('#Population').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
+
+$('#Country').change(function () {
+    Validate();
+})
+
+$('#City').change(function () {
+    Validate();
+})
+
+$('#Population').change(function () {
+    Validate();
+})
+
+//Edit
+function Edit(id) {
+    $.ajax({
+        url: '/Country/Edit?id=' + id,
+        type: 'get',
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (response) {
+            if (response == null || response == undefined) {
+                alert('Unable read to data');
+            }
+            else if (response.length == 0) {
+                alert('Data not available with the id' + id);
+            }
+            else {
+                $('#CountryModal').modal('show');
+                $('modalTitle').text("Update Country");
+                $('#Save').css('display', 'none');
+                $('#Update').css('display', 'block');
+                $('#Id').val(response.id);
+                $('#Id').val(response.country);
+                $('#Id').val(response.city);
+                $('#Id').val(response.population);
+                $('#Id').val(response.capitalCity);
+            }
+        },
+        error: function () {
+            alert('Unable read to data');
+        }
+    });
+}
+
+//Update Data
+function Update() {
+    var result = Validate();
+    if (result == false) {
+        return false;
+    }
+    var formData = new object();
+    formData.id = $('#Id').val();
+    formData.Country = $('#Country').val();
+    formData.City = $('#City').val();
+    formData.Population = $('#Population').val();
+    formData.CapitalCity = $('#CapitalCity').val();
+
+    $.ajax({
+        url: '/Country/Update',
+        data: formData,
+        type: 'Post',
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                alert('Unable to save the data');
+            }
+            else {
+                HideModal();
+                ShowAllData();
+                alert(response);
+            }
+        },
+        error: function () {
+            alert('Unable to save the data');
+        }
+    }
+
+//Delete Data
+    function Delete(id) {
+        if (confirm("Are you sure to delete this record?")) {
+            $.ajax({
+                url: '/Country/Delete?id=' + id,
+                type: 'post',
+                success: function (response) {
+                    if (response == null || response == undefined) {
+                        alert('Unable to delete to data');
+                    }
+
+                    else {
+                        ShowAllData();
+                        alert(response);
+                    }
+                },
+                error: function () {
+                    alert('Unable to delete to data');
+                }
+            });
+        }
+            
+        }
+
+
+//function DelteId(id) {
+//    $.ajax({
+//        url: `Country/Delete/${id}`,
+//        type: "Delete"
+//    })
+//}
+
+//function EditId() {
+//    window.location.href = "/country/create";
+//    $.ajax({
+//        url: 'Country/Edit',
+//        type: "Get"
+//    })
+
+//}
